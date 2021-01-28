f46d808c147fd547566872178768ceb2b7178a33
Dan Schultz <dan.schultz@vertexvis.com>
2021-01-28 15:05:08 -0600

    Fix lint errors

--- packages/build-tools/src/typings/plugins.d.ts
+++ packages/build-tools/src/typings/plugins.d.ts
@@ -1,6 +1,5 @@
-/* eslint-disable */
-
 declare module 'rollup-plugin-auto-external' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const autoExternal: any;
   export default autoExternal;
 }
