ee3c6fe9da6a88d7b65216b90eba4ca7fe8847e0
Dan Schultz <dan.schultz@vertexvis.com>
2021-05-10 12:40:47 -0500

    Fix incorrect package detection in publish script

--- scripts/post_download_artifacts.sh
+++ scripts/post_download_artifacts.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-packages=`cat lerna.json | jq -r '.packages[]'`
+packages=`cat package.json | jq -r '.workspaces[]'`
 package_directories=($packages)
 
-for package_path in "${package_directories[@]}"; do 
+for package_path in "${package_directories[@]}"; do
   dist_directory="dist/$package_path/dist"
 
   if test -d $dist_directory; then
     cp -r $dist_directory/ $package_path/dist
-  else 
+  else
     echo "Package at path $package_path does not have a dist directory. Skipping."
   fi
 done
--- scripts/pre_upload_artifacts.sh
+++ scripts/pre_upload_artifacts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-packages=`cat lerna.json | jq -r '.packages[]'`
+packages=`cat package.json | jq -r '.workspaces[]'`
 package_directories=($packages)
 
 mkdir dist
 
-for package_path in "${package_directories[@]}"; do 
+for package_path in "${package_directories[@]}"; do
   dist_directory="$package_path/dist"
 
   if test -d $dist_directory; then
